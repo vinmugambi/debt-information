643fff317dfff50a5e4f5efbf5fe35479f7fc234
vinmugambi <vinmugambi17@gmail.com>
2017-05-16 16:11:48 +0300

    Added error handlers for 404 and 500

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 var express = require('express'),
     app = express(),
-    setupPassport = require('./app/config/passport'),
+    passport = require('./app/config/passport'),
     flash = require('connect-flash'),
-    appRouter = require('./app/routes')(express),
+    routes = require('./app/routes')(express),
     session = require('express-session'),
     bodyParser = require('body-parser'),
     cookieParser = require('cookie-parser'),
 		exphbs=require("express-handlebars"),
-		path=require("path"),
-    jsonParser = bodyParser.json()
+		path=require("path");
 
-var port = process.env.PORT || 8080
-
+app.set("port",process.env.PORT || 3000 )
 app.use(cookieParser())
-app.use(session({ secret: '4564f6s4fdsfdfd', resave: false, saveUninitialized: false }))
+app.use(session({ secret: '&TYUTufkdu8727910hsh', resave: false, saveUninitialized: false }))
 app.engine('handlebars', exphbs({
   defaultLayout: 'main',
   helpers: {
@@ -38,23 +36,31 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
-
+//flashing errors and notifications
 app.use(flash())
 app.use(function(req, res, next) {
     res.locals.errorMessage = req.flash('error')
     next()
 });
-app.use(jsonParser)
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }))
 
-setupPassport(app)
+passport(app);
+app.use('/', routes);
 
-app.use('/', appRouter)
+app.use(function (req, res) {
+  res.status(404);
+  res.render('404');
+});
+// custom 500 page
+app.use(function (err, req, res) {
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
 
 // start app
-app.listen(port)
-console.log('Server started on port ' + port)
-
-module.exports.getApp = app
+app.listen(app.get('port'));
+console.log("Application is running on port %s ",app.get('port'));
