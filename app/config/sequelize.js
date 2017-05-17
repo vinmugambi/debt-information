864dcb91a285d1f6e7cc81741ec0d87d36244a70
vinmugambi <vinmugambi17@gmail.com>
2017-05-17 16:27:08 +0300

    changed postgres connection string

--- app/config/sequelize.js
+++ app/config/sequelize.js
@@ -4,6 +4,6 @@ const Sequelize = require("sequelize");
 *Change the connection string to suit your database name, password, port ,host address,and user
 *ie const sequelize = new Sequelize('postgres://db_username:db_password@db_host:db_port/db_name')
 */
-const sequelize = new Sequelize('postgres://debt:mugambi@localhost:5432/used')
+const sequelize = new Sequelize('postgres://dev001​:EV5gy2pQPDhC4H&fg3$5qzWL*9P4=D2K8ta9x&Qr2@51.140.33.76:​6773​/testdb')
 
 module.exports = sequelize
